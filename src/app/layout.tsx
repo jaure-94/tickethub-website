76b70af145570b0b70cf9f5075a8044659563107
Tawanda Jaure <tawandajaujau@gmail.com>
2025-02-22 23:46:59 +0200

    clerk auth integration with src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Pacifico, Kanit, Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
 
 const kanit = Kanit({
   variable: "--font-kanit",
@@ -31,12 +32,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${kanit.variable} ${poppins.variable} ${pacifico.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${kanit.variable} ${poppins.variable} ${pacifico.variable} h-screen flex justify-center items-center antialiased`}
+        >
+          <SignedOut>
+            <SignIn routing="hash" />
+          </SignedOut>
+          
+          <SignedIn>
+            {children}
+          </SignedIn>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,10 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import { FC } from 'react'
+import logo from '@/assets/images/logo.svg'
+import { UserButton } from '@clerk/nextjs'
 
 type Props = {}
 
 const Header: FC = (props: Props) => {
   return (
-    <div>Header</div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src={logo} alt="TicketHub Logo" width={128} height={38} />
+        </Link>
+
+        <UserButton showName />
+
+        <div className="flex w-32 justify-end gap-3">
+          <div></div>
+        </div>
+      </div>
+    </header>
   )
 }
 
