ff91e28cf9bbd080d792ff4f61a96f867cf55e95
Tawanda Jaure <tawandajaujau@gmail.com>
2025-02-24 18:23:55 +0200

    footer responsive styling

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link'
 import { FC } from 'react'
 
 type Props = {}
 
 const Footer: FC = (props: Props) => {
   return (
-    <div>Footer</div>
+    <footer className="wrapper flex py-10 flex-between flex-col md:flex-row gap-5 items-center border-t">
+      <Link href="/">
+        <h2 className="font-bold text-lg">TicketHub ZW</h2>
+      </Link>
+      <p className="text-xs text-gray-500 font-thin">&copy; TicketHub ZW - 2025</p>
+    </footer>
   )
 }
 
