a9dda3a0908691d920eef906a4ddcd5875b40054
Tawanda Jaure <tawandajaujau@gmail.com>
2025-03-02 08:59:07 +0200

    set up webhook to create new user in database
    > every time a new user is created via clerk auth and login, we use
      webhook (svix) to cature that event and add the user to our
      mongodb database

---
+++ src/lib/actions/user.actions.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import { CreateUserParams } from "@/types"
+import { handleError } from "@/lib/utils"
+import { connectToDatabase } from "@/lib/mongodb/database"
+import User from "../mongodb/database/models/user.model"
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    await connectToDatabase()
+
+    const newUser = await User.create(user)
+
+    return JSON.parse(JSON.stringify(newUser))
+  } catch (error) {
+    handleError
+  }
+}
--- src/lib/mongodb/database/models/user.model.ts
+++ src/lib/mongodb/database/models/user.model.ts
@@ -1,9 +1,19 @@
 import { model, models, Schema } from "mongoose";
 
+export interface IUser extends Document {
+  _id: string;
+  email: string;
+  username: string;
+  firstname: string;
+  lastname: string;
+}
+
 const UserSchema = new Schema({
   clerckId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
+  firstname: { type: String, required: true, unique: true },
+  lastname: { type: String, required: true, unique: true },
 });
 
 const User = models.User || model('User', UserSchema);
